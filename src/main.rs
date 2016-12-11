ab2378935246008d59d5421a655f632f548e5089
lethargilistic <mikeoverby@outlook.com>
2016-12-11 17:23:46 -0500

    breaking response into a repeating fn

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,27 @@
 extern crate reqwest;
 
 use std::io::Read;
+use std::time::Duration;
+use std::thread;
 
-fn get_ip() -> String {
-    
+fn get_response(tries_remaining:i32) -> reqwest::Response {
 	let mut res = match reqwest::get("https://api.ipify.org") {
         Ok(val) => val,
-        Err(err) => panic!("There was a network error when trying to connect to ipify.")
+        Err(err) => 
+            if tries_remaining > 0{
+                thread::sleep(Duration::from_millis(500));
+                return get_response(tries_remaining-1);
+            }
+            else{
+                panic!("There was a network error when trying to connect to ipify.");
+            }
     };
+
+    res
+}
+fn get_ip() -> String {
+    let mut res = reqwest::get("https://api.ipify.org").unwrap() ;//get_response(3);
+    
 	assert!(res.status().is_success(), "Received an invalid status from ipify");
 
 	let mut ip = String::new();
