3728410464139a43f4706b5815685ecc918ede0c
lethargilistic <mikeoverby@outlook.com>
2016-12-11 16:07:06 -0500

    Added network message error

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,12 @@ extern crate reqwest;
 use std::io::Read;
 
 fn get_ip() -> String {
-	let mut res = reqwest::get("https://api.ipify.org").unwrap();
-	assert!(res.status().is_success(), "Recieved an invalid status from ipify");
+    
+	let mut res = match reqwest::get("https://api.ipify.org") {
+        Ok(val) => val,
+        Err(err) => panic!("There was a network error when trying to connect to ipify.")
+    };
+	assert!(res.status().is_success(), "Received an invalid status from ipify");
 
 	let mut ip = String::new();
 	res.read_to_string(&mut ip).unwrap();
