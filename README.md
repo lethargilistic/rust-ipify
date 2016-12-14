64f6350569c94d7919eda6917592bbc4c4e3a064
lethargilistic <mikeoverby@outlook.com>
2016-12-13 19:02:10 -0500

    Breaking ipify into mod file

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# rust-ipify
+# ipify_rust
 A client library for ipify in Rust. The best way to get your IP.
---
+++ src/ipify.rs
@@ -0,0 +1,45 @@
+pub mod ipify {
+    extern crate reqwest;
+
+    use std::io::Read;
+    use std::time::Duration;
+    use std::thread;
+
+    const MAX_RETRIES: i32 = 3;
+    const RETRY_WAIT_TIME : u64 = 500;
+    static API_URL: &'static str = "https://api.ipify.org";
+
+    fn get_response(tries_remaining:i32) -> reqwest::Response {
+        match reqwest::get(API_URL) {
+            Ok(response) => response,
+            Err(err) => 
+                if tries_remaining > 0 {
+                    thread::sleep(Duration::from_millis(RETRY_WAIT_TIME));
+                    get_response(tries_remaining-1)
+                }
+                else{
+                    panic!("There was a network error when trying to connect to ipify.\n{}", err);
+                }
+        }
+    }
+
+    pub fn get_ip() -> String {
+        let mut res = get_response(MAX_RETRIES);
+        
+        assert!(res.status().is_success(), "Received an invalid status from ipify");
+
+        let mut ip = String::new();
+        res.read_to_string(&mut ip).unwrap();
+
+        ip
+    }  
+
+    #[test]
+    fn test_get_ip() {
+        use std::net::SocketAddr;
+
+        let ip = get_ip() + ":8080"; //Needs a port number
+
+        let server: SocketAddr = ip.parse().expect("The IP address is invalid");
+    }
+}
